e03a924c54788c68ae8a7573fad307a28fc9b2b3
dev <maxikavia@mail.ru>
2022-11-12 17:36:15 +0300

    develop front continu

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'shop'
+    'shop',
+    'crispy_forms',
+    'accounts'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
